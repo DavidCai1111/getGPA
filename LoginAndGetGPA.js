fb14136a77b23bb9d026a52b4b4e0c5b812c9f16
davidcai <376462191@qq.com>
2015-03-16 18:37:12 +0800

    获取GPA功能完成

--- LoginAndGetGPA.js
+++ LoginAndGetGPA.js
@@ -3,18 +3,45 @@ var casper = require("casper").create({
     logLevel:"info"
 });
 var utils = require("utils");
+var fs = require("fs");
 
 console.log(" ");
 
 casper.start("http://jwc1.usst.edu.cn",function(res){
     utils.dump(res);
+    this.capture("layout.jpg");
 });
 
 casper.then(function(){
-    this.capture("layout.jpg");
     this.captureSelector('checkCode.jpg','dd img[src="CheckCode.aspx"]',{
         quality:100
     });
+});
+
+
+casper.waitFor(function check() {
+    return fs.exists('result.txt');
+}, function then() {
+    var checkCode = fs.read('result.txt').trim();
+    console.log("checkCode: ");
+    console.log(checkCode);
+
+    this.fill('form#form1',{
+        'TextBox1':'1213490138',
+        'TextBox2':'c65655650',
+        'TextBox3':checkCode
+    },false);
+
+    this.capture("before.jpg");
+
+});
+
+casper.then(function(){
+    this.click('#Button1');
+});
+
+casper.then(function(){
+    this.capture("afterAll.jpg");
 });
 
 casper.then(function(){
--- getGPA.js
+++ getGPA.js
@@ -4,11 +4,31 @@ var fs = require('fs');
 
 console.log("start!");
 
-execSync('casperjs LoginAndGetGPA.js');
+//删除残留文件
+if(fs.existsSync("result.txt")){
+    fs.unlinkSync("result.txt");
+}
+if(fs.existsSync("layout.jpg")){
+    fs.unlinkSync("layout.jpg");
+}
+if(fs.existsSync("checkCode.jpg")){
+    fs.unlinkSync("checkCode.jpg");
+}
+if(fs.existsSync("checkCodeFiltered.jpg")){
+    fs.unlinkSync("checkCodeFiltered.jpg");
+}
 
+exec('casperjs LoginAndGetGPA.js',function(){
+    console.log("end!");
+});
+
+//等待获取到验证码
+while(!fs.existsSync("checkCode.jpg")){
+
+}
+
+//获取验证码文本
 execSync('java MyImgFilter');
+execSync('tesseract checkCodeFiltered.jpg result -l eng  -psm 7');
 
-execSync('tesseract checkCodeFiltered.jpg result');
-
-console.log("end!");
 
