e38376210346cfd5bb72bdc2cbc5b65606847383
davidcai <376462191@qq.com>
2015-03-17 11:56:46 +0800

    循环尝试

--- getGPA.js
+++ getGPA.js
@@ -2,23 +2,22 @@ var execSync  = require('child_process').execSync;
 var exec = require('child_process').exec;
 var fs = require('fs');
 
-console.log("start!");
-
-console.log("一共传入了" + (process.argv.length -2 ) + "个参数" );
-for(var i = 2 ; i < process.argv.length ; i++){
-    console.log("第" + (i-1) + "个参数是" + process.argv[i]);
-}
-
 //若参数数量不是两个，则提示并退出
-if(process.argv.length !==4 ){
+if((process.argv.length - 2) !== 2  ){
     console.log("输入的参数数量不对，退出");
     process.exit(1);
 }
 
+console.log("输入的用户名为：" +  process.argv[2]);
+console.log("输入的密码为：" +  process.argv[3]);
+
+//保存用户名密码
 var userToLogin = {
-    name:process.argv[2],
+    username:process.argv[2],
     password:process.argv[3]
 };
+
+console.log("开始登录!");
 
 function getGPA(){
     //删除残留通信文件
@@ -39,23 +38,27 @@ function getGPA(){
     }
 
     //执行登录
-    exec('casperjs LoginAndGetGPA.js',function(){
+    exec(('casperjs LoginAndGetGPA.js '+ userToLogin.username + " " + userToLogin.password ),function(){
         if(fs.readFileSync('result.txt').toString() === "success"){
             //尝试成功
-            console.log("end!");
+            console.log("登录成功!");
+        }else if(fs.existsSync("alertedMessage.txt") && fs.readFileSync("alertedMessage.txt").toString() === "密码错误！"){
+            //密码错误
+            console.log("输入的密码错误!");
+            process.exit(1);
         }else{
             //尝试失败，重新尝试
-            console.log("try again!");
+            console.log("登录中...");
             getGPA();
         }
     });
 
-    //等待获取到验证码
+    //等待获取到验证码图片
     while(!fs.existsSync("checkCode.jpg")){
 
     }
 
-    //获取验证码文本
+    //预处理，分析获取验证码文本
     execSync('java MyImgFilter');
     execSync('tesseract checkCodeFiltered.jpg checkCodeText -l eng  -psm 7');
 }
