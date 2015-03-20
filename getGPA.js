8660a662e51e818f10d7199d25c163bf9697ed52
DavidCai <376462191@qq.com>
2015-03-21 00:44:15 +0800

    优化

--- getGPA.js
+++ getGPA.js
@@ -20,28 +20,6 @@ var userToLogin = {
 console.log("[getGPA] 开始登录!");
 
 function getGPA(){
-    //删除残留通信文件
-    if(fs.existsSync("result.txt")){
-        fs.unlinkSync("result.txt");
-    }
-    if(fs.existsSync("checkCodeText.txt")){
-        fs.unlinkSync("checkCodeText.txt");
-    }
-    if(fs.existsSync("layout.jpg")){
-        fs.unlinkSync("layout.jpg");
-    }
-    if(fs.existsSync("checkCode.jpg")){
-        fs.unlinkSync("checkCode.jpg");
-    }
-    if(fs.existsSync("checkCodeFiltered.jpg")){
-        fs.unlinkSync("checkCodeFiltered.jpg");
-    }
-    if(fs.existsSync("GPA.txt")){
-        fs.unlinkSync("GPA.txt");
-    }
-    if(fs.existsSync("alertedMessage.txt")){
-        fs.unlinkSync("alertedMessage.txt");
-    }
 
     //执行登录
     exec(('casperjs LoginAndGetGPA.js '+ userToLogin.username + " " + userToLogin.password ),function(){
@@ -53,6 +31,29 @@ function getGPA(){
             }
             var GPA = fs.readFileSync('GPA.txt').toString();
             console.log("[getGPA] 结果：" + GPA);
+
+            //删除残留通信文件
+            if(fs.existsSync("result.txt")){
+                fs.unlinkSync("result.txt");
+            }
+            if(fs.existsSync("checkCodeText.txt")){
+                fs.unlinkSync("checkCodeText.txt");
+            }
+            if(fs.existsSync("layout.jpg")){
+                fs.unlinkSync("layout.jpg");
+            }
+            if(fs.existsSync("checkCode.jpg")){
+                fs.unlinkSync("checkCode.jpg");
+            }
+            if(fs.existsSync("checkCodeFiltered.jpg")){
+                fs.unlinkSync("checkCodeFiltered.jpg");
+            }
+            if(fs.existsSync("GPA.txt")){
+                fs.unlinkSync("GPA.txt");
+            }
+            if(fs.existsSync("alertedMessage.txt")){
+                fs.unlinkSync("alertedMessage.txt");
+            }
         }else if(fs.existsSync("alertedMessage.txt") && fs.readFileSync("alertedMessage.txt").toString() === "密码错误！"){
             //密码错误
             console.log("[getGPA] 输入的密码错误!");
